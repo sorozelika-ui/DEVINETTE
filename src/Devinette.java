fada571840a9d571171250a2150f450e0e9578cb
sorowagniguizelika <sorozelika@gmail.com>
2026-05-10 19:28:22 +0000

    ajout des boucles

--- src/Devinette.java
+++ src/Devinette.java
@@ -12,5 +12,44 @@ public class Devinette {
         // Création du scanner
         Scanner scanner = new Scanner(System.in);
 
+        int tentative = 10;
+
+        System.out.println("Devinette");
+
+           //10 tentatives de devienttes
+        for (int i = 1; i <= tentative; i++) {
+
+            System.out.println("Tentative " + i);
+            System.out.println("Entrez votre nombre :");
+
+            int grade = scanner.nextInt();
+
+            if (grade == nb) {
+
+                System.out.println("Wow ! Bien joué !");
+                break;
+
+            } else {
+
+                if (grade > nb) {
+
+                    System.out.println("Nombre trop grand");
+
+                } else {
+
+                    System.out.println("Nombre trop petit");
+                }
+            }
+
+
+            if (i == tentative) {
+
+                System.out.println("Pas mal, mais tu peux faire mieux la prochaine fois !");
+
+            }
+        }
+
+        scanner.close();
+
     }
 }
