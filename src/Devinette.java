ccc014509e259b96702999cfa7b6f3fc02c19615
sorowagniguizelika <sorozelika@gmail.com>
2026-05-10 19:15:32 +0000

    import des bibliotheques et declaration des variables

---
+++ src/Devinette.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Devinette {
+
+    public static void main(String[] args) {
+
+        // Génération du nombre aléatoire entre 1 et 100
+        Random random = new Random();
+        int nb = random.nextInt(100) + 1;
+
+        // Création du scanner
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
